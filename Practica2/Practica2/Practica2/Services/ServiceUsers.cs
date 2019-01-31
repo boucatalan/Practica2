ba83d88379e7136d0ada689b9a38d923ae80645e
boucatalan <davidbou1996@gmail.com>
2019-01-31 12:28:21 +0100

    Services

--- Practica2/Practica2/Practica2/Services/ServiceUsers.cs
+++ Practica2/Practica2/Practica2/Services/ServiceUsers.cs
@@ -6,7 +6,14 @@ namespace Practica2.Services
 {
     class ServiceUsers
     {
-        private List<User> _usuarios = new List<User>();
+        private List<Usuario> _usuarios = new List<Usuario>();
+        Usuario user1 = new Usuario(01, "David", "Bou", "Catalán", "45990250W", 660605534, "davidbou1996@gmail.com");
+        Usuario user2 = new Usuario(02, "Joel", "Caso", "Blanca", "45990251A", 666666666, "joelCB@gmail.com");
 
+        public static void rellenaListaUsuarios()
+        {
+            _usuarios.Add(user1);
+            _usuarios.Add(user2);
+        }
     }
 }
